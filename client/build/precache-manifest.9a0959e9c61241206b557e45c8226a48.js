9374cbd3584f22682165d3ad2161f51dc29e17f3
Amin Bouzouita <Aminbouzouita@yahoo.com>
2020-11-21 22:28:06 -0800

    install build

---
+++ client/build/precache-manifest.9a0959e9c61241206b557e45c8226a48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fb83c792333837dbafcd62ef26d4209",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c101adeafa450e41e275",
+    "url": "/static/css/main.d1b26f36.chunk.css"
+  },
+  {
+    "revision": "9140b49c326de531c7aa",
+    "url": "/static/js/2.2e1909cf.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.2e1909cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c101adeafa450e41e275",
+    "url": "/static/js/main.ce74ed5f.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
