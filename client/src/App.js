0a0f81c5ba24df53efec59fa803303dcc456e0b0
Amin Bouzouita <Aminbouzouita@yahoo.com>
2020-11-21 22:31:23 -0800

    add router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SavedBooks from "./pages/SavedBooks";
+import SearchBooks from "./pages/SearchBooks";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
 
 function App() {
- 
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path={["/", "/books"]}>
+            <SavedBooks />
+          </Route>
+          <Route exact path="/books/Search">
+            <SearchBooks />
+          </Route>
+          <Route exact path="/books/:id">
+            <Detail />
+          </Route>
+          <Route>
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
